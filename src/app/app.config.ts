5f4a179250969b585bf2f8c8a6804b183fb1a335
NotFabianML <80232944+NotFabianML@users.noreply.github.com>
2025-07-19 15:41:27 -0600

    feat: Add primeng toast

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -8,10 +8,11 @@ import { baseUrlInterceptor } from './interceptors/base-url.interceptor';
 import { accessTokenInterceptor } from './interceptors/access-token.interceptor';
 import { handleErrorsInterceptor } from './interceptors/handle-errors.interceptor';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { MessageService } from 'primeng/api';
 
 export const appConfig: ApplicationConfig = {
   providers: [
-    provideRouter(routes), 
+    provideRouter(routes),
     provideClientHydration(),
     provideHttpClient(
       withInterceptors([
@@ -19,6 +20,7 @@ export const appConfig: ApplicationConfig = {
         accessTokenInterceptor,
         //handleErrorsInterceptor
       ])
-    ), provideAnimationsAsync()
+    ), provideAnimationsAsync(),
+    MessageService,
   ]
 };
