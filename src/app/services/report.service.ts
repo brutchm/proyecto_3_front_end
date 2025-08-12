d13b1ddd53d3835d917c5f25c3034fa55fd29eeb
NotFabianML <80232944+NotFabianML@users.noreply.github.com>
2025-08-12 13:17:18 -0600

    minor change

--- src/app/services/report.service.ts
+++ src/app/services/report.service.ts
@@ -8,17 +8,7 @@ import { GlobalResponse } from '../interfaces/GlobalResponse.interface';
 })
 export class ReportService {
   private http: HttpClient = inject(HttpClient);
-  private readonly baseUrl = 'dashboard/reports'; // Apunta al endpoint de reportes
-
-  /**
-   * Genera un reporte enviando la configuración al backend.
-   * @param reportConfig - El objeto con el tipo de reporte y los filtros.
-   * @returns Un Observable con los datos del reporte.
-   */
-//   generateReport(reportConfig: any): Observable<IIncomeVsExpensesData> {
-//     return this.http.post<GlobalResponse<IIncomeVsExpensesData>>(this.baseUrl, reportConfig)
-//       .pipe(map(response => response.data));
-//   }
+  private readonly baseUrl = 'dashboard/reports';
 
   /**
    * Genera un reporte enviando la configuración al backend.
