353c28e398599f3d19ae7c450fc8264cefd1965b
Sthephanie-Leon-Vargas <sleonv@ucenfotec.ac.cr>
2025-08-13 10:37:31 -0600

    feat: Validacion simple de caracteres permitidos
    Se restringe el campo a 500 caracteres permitidos para el campo textarea en donde el usuario escribe su pregunta al usuario

--- src/app/pages/suggestionAI/suggestionAI.component.ts
+++ src/app/pages/suggestionAI/suggestionAI.component.ts
@@ -42,7 +42,8 @@ export class SuggestionAIComponent implements OnInit {
   displayedText = "";
   private messageService: MessageService = inject(MessageService);
   isSuggestionSaved: boolean = false;
-
+  maxChars = 500;//limite de caracteres
+  charCount = 0;
   constructor(
     private fb: FormBuilder,
     private suggestionService: SuggestionAIService,
@@ -51,9 +52,14 @@ export class SuggestionAIComponent implements OnInit {
 
   ngOnInit() {
     this.suggestionForm = this.fb.group({
-      prompt: ["", Validators.required],
+      prompt: ["", [Validators.required, Validators.maxLength(this.maxChars)]],
       farmId: [null, Validators.required],
     });
+
+    this.suggestionForm.get('prompt')?.valueChanges.subscribe(value => {
+      this.charCount = value?.length || 0;
+    });
+  
 
     this.loadFarms();
   }
